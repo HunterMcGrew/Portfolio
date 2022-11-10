83eb7790a1df8928bc06536a7b6dc2716b8a4126
Hunter <hunter109@gmail.com>
2022-11-09 22:02:18 -0600

    added icon link and fixed an import typo in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./reset.css";
 import './App.css';
-import "/.main.css";
+import "./main.css";
 import Header from './components/header/Header';
 
 function App() {
